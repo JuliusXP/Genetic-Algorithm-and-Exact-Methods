15f611d63937d855c2c52e67952a19f0f9f64372
JuliusXP <julivargred@gmail.com>
2026-06-24 05:15:47 -0600

    Minus Changes (Base Implemented)
    The implementation of the base class to have modular programming with polimorfic functions, in this case createIndividual and Fitness, and also have the implementation of the first problem, in this case the subsetSum with the modifications of the needed functions and working in the main file

---
+++ classes/classSubsetSum.py
@@ -0,0 +1,29 @@
+import classBase
+import random
+
+# Subclass for the Subset Sum problem, inheriting from the Base class
+class SubsetSum(classBase.Base):
+    def __init__(self, numbers, target, selectionType, populationSize, generations, tournamentSize, mutationProb, crossoverProb, elitismNum, patience):
+        super().__init__(selectionType, populationSize, generations, tournamentSize, mutationProb, crossoverProb, elitismNum, patience)
+        self.numbers = numbers
+        self.target = target
+
+    #Create an Individual chromosome, which is a binary representation of the subset selection
+    def createIndividual(self):
+        individual = []
+
+        for i in range(len(self.numbers)):
+            individual.append(random.randint(0,1))
+
+        return individual
+
+    #Calculate the fitness of an individual, which is based on how close the sum of the selected numbers is to the target sum
+    def fitness(self, individual):
+        total = 0
+
+        for i in range(len(individual)):
+            if individual[i] == 1:
+                total += self.numbers[i]
+
+        difference = abs(self.target - total)
+        return 1 / (1 + difference)
