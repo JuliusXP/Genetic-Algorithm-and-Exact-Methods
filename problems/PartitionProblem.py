d18ef10f8914e620b0c6ba257de7f0a762641b1b
Warlos123 <carlitos.manuel01@gmail.com>
2026-07-04 17:06:33 -0600

    Added Bottom Up to partition and subset

--- problems/PartitionProblem.py
+++ problems/PartitionProblem.py
@@ -172,3 +172,39 @@ class PartitionProblem:
 
         # Print the results of the genetic algorithm
         self.genetic.printResults(result, numbers)
+
+    #Bottom up 
+    def BottomUpequalPartition(arr):
+    
+        # Calculate sum of the elements in array
+        arrSum = sum(arr)
+        n = len(arr)
+
+        # If sum is odd, there cannot be two 
+        # subsets with equal sum
+        if arrSum % 2 != 0:
+            return False
+
+        arrSum = arrSum // 2
+
+        # Create a 2D array for storing results
+        # of subproblems
+        dp = [[False] * (arrSum + 1) for _ in range(n + 1)]
+
+        # If sum is 0, then answer is true (empty subset)
+        for i in range(n + 1):
+            dp[i][0] = True
+
+        # Fill the dp table in bottom-up manner
+        for i in range(1, n + 1):
+            for j in range(1, arrSum + 1):
+                if j < arr[i - 1]:
+                
+                    # Exclude the current element
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                
+                    # Include or exclude
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+
+        return dp[n][arrSum]
--- problems/SubsetSum.py
+++ problems/SubsetSum.py
@@ -149,3 +149,31 @@ class SubsetSum:
 
         # Print the results of the genetic algorithm
         self.genetic.printResults(result, numbers, target)
+
+
+    #Bottom up 
+    def BottomUpisSubsetSum(arr, sum):
+        n = len(arr)
+
+        # Create a 2D list for storing 
+        # results of subproblems
+        dp = [[False] * (sum + 1) for _ in range(n + 1)]
+
+        # If sum is 0, then answer is 
+        # true (empty subset)
+        for i in range(n + 1):
+            dp[i][0] = True
+
+        # Fill the dp table in bottom-up manner
+        for i in range(1, n + 1):
+            for j in range(1, sum + 1):
+                if j < arr[i - 1]:
+                    
+                    # Exclude the current element
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    
+                    # Include or exclude
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+
+        return dp[n][sum]
